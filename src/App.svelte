f8849fe0c95e6177c2ef2ec75d0ca3c422019553
Gavin Rehkemper <gavin@gavinr.com>
2021-09-04 00:13:50 -0500

    svelte update - basic map app

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,13 @@
-<script>
-  import { onMount } from "svelte";
-  import Map from "./Map.svelte";
-  import List from "./List.svelte";
-  import Info from "./Info.svelte";
-  import { crimesStore, startUpdating } from './stores';
-  export let name;
-  export let selectedCrime = false;
-  export let centerMap = false;
-
-  onMount(async function() {
-    startUpdating();
-  });
-
-  function listClickHandler(evt, c) {
-    if (selectedCrime == evt.detail || evt.detail === false) {
-      selectedCrime = false; // toggle
-    } else {
-      centerMap = c;
-      selectedCrime = evt.detail;
-    }
-  }
+<script lang="ts">
+  import EsriMapView from "./lib/EsriMapView.svelte";
+  import "./style.css";
 </script>
 
+<EsriMapView />
+
 <style>
-  header {
-    grid-column: 1 / span 5;
-    font-size: 40px;
-    padding: 10px;
-    overflow: hidden;
-  }
-  header span {
-    float: right;
-  }
-
-  aside {
-    grid-column: span 2;
-    overflow-y: scroll;
-  }
-
-  @media (max-width: 700px) {
-    aside {
-      grid-column: 1 / span 5;
-    }
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
 </style>
-
-
-<header>
-  {name}
-  <span><Info {name}></Info></span>
-</header>
-<Map {selectedCrime} {centerMap} on:selected={ (e) => {listClickHandler(e, false)} } />
-<aside id="sidebar">
-  <List {selectedCrime} on:click={ (e) => {listClickHandler(e, true)} } />
-</aside>
---
+++ src/lib/EsriMapView.svelte
@@ -0,0 +1,23 @@
+<script>
+    import MapView from "@arcgis/core/views/MapView";
+
+    let view;
+
+    const createMapView = (domNode) => {
+        view = new MapView({
+            container: domNode,
+            map: {
+                basemap: "gray",
+            },
+        });
+    };
+</script>
+
+<div use:createMapView />
+
+<style>
+    div {
+        height: 100%;
+        width: 100%;
+    }
+</style>
