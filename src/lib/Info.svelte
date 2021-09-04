52e4fc3a542f14f6fabf8d786c2de05f5a5b14b8
Gavin Rehkemper <gavinr@users.noreply.github.com>
2021-09-04 19:08:36 +0000

    Svelte (using Vite) version (#21)
    * svelte update - basic map app
    
    * updates
    
    * gha
    
    * cleanup

---
+++ src/lib/Info.svelte
@@ -0,0 +1,33 @@
+<script>
+    import Swal from "sweetalert2";
+    import "sweetalert2/dist/sweetalert2.css";
+
+    export let name = "";
+
+    const showInfo = () => {
+        Swal.fire({
+            title: `About ${name}`,
+            html: 'This is a live view of the City of St. Louis Police Department <a href="http://www.slmpd.org/cfs.aspx" target="_blank">calls for service</a>. These are just requests - none of these crimes have been verified, charged, or prosecuted. Please leave feedback this project here: <a href="https://github.com/gavinr/stl-live-crime" target="_blank">github.com/gavinr/stl-live-crime</a>.',
+            icon: "info",
+            confirmButtonText: "Cool",
+        });
+    };
+</script>
+
+<span on:click={showInfo}
+    ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        ><path
+            d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9a1 1 0 0 1 1 1v4a1 1 0 0 1-2 0v-4a1 1 0 0 1 1-1zm0-4a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+        /></svg
+    ></span
+>
+
+<style>
+    span {
+        cursor: pointer;
+    }
+</style>
---
+++ src/lib/stores.ts
@@ -0,0 +1,98 @@
+import { writable } from 'svelte/store';
+
+import Geocoder from "./Geocoder";
+
+import type { Crime } from './types';
+
+export const crimesStore = writable<Crime[]>([]);
+
+
+export const startUpdating = async () => {
+    const values = await getCrimes();
+    crimesStore.update(arr => {
+        return values;
+    });
+
+    setInterval(async () => {
+        const values = await getCrimes();
+        console.log('updating', values);
+        crimesStore.update(arr => {
+            return values;
+        });
+    }, 60000);
+}
+
+const getSize = date => {
+    let difference = Math.abs((new Date()).getTime() - (new Date(date)).getTime());
+    let retSize = 10;
+    if (difference < 3600000) {
+        // 1 hour
+        retSize = 30;
+    } else if (difference < 7200000) {
+        // 2 hours
+        retSize = 20;
+    }
+
+    return retSize;
+};
+
+const querySelectorAllFunction = (query, context) => {
+    console.log('querySelectorAllFunction query:', query, 'context: ', context);
+
+    const res = Array.prototype.slice.call(context.querySelectorAll(query));
+    console.log('res', res);
+    return res;
+};
+
+const getCrime = async (row): Promise<Crime> => {
+    console.log('getCrime', row);
+    const [date, id, address, offense] = querySelectorAllFunction(
+        "td",
+        row
+    ).map(cell => {
+        return cell.querySelector("font").innerHTML;
+    });
+
+    const fullAddress =
+        address.replace("XX ", "00 ").replace(" / ", " and ") +
+        ", St. Louis, MO, USA";
+
+    const location = await Geocoder(fullAddress);
+    let lat = false;
+    let lon = false;
+    if (location.locations.length > 0) {
+        if (location.locations[0].feature.attributes.Score > 90.0) {
+            lat = location.locations[0].feature.geometry.y;
+            lon = location.locations[0].feature.geometry.x;
+        }
+    }
+
+    const c: Crime = {
+        date: date,
+        id: id,
+        address: fullAddress,
+        offense: offense,
+        size: getSize(date),
+        lat: lat,
+        lon: lon
+    };
+
+    return c;
+};
+
+const getCrimes = async (): Promise<Crime[]> => {
+    console.log('getCrimes');
+    const response = await fetch(
+        "https://jsonp.afeld.me?url=http://www.slmpd.org/cfs.aspx"
+    );
+    const text = await response.text();
+
+    // Parse the result from the request into a DOM document so we can parse through it and turn it into JSON
+    var doc = new DOMParser().parseFromString(text, "text/html");
+
+    const rows = querySelectorAllFunction("table tbody tr", doc);
+    const rowsPromises: Promise<Crime>[] = rows.map(getCrime);
+    const values = Promise.all(rowsPromises);
+
+    return values;
+};
