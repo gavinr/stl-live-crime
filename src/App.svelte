c4f5e18bf14a1eaec37f6760caa3e39c52ef2f43
Gavin Rehkemper <gavin@gavinr.com>
2021-09-04 12:21:26 -0500

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,71 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import type { Crime } from "./lib/types";
   import EsriMapView from "./lib/EsriMapView.svelte";
+  import Info from "./lib/Info.svelte";
+  import List from "./lib/List.svelte";
+
+  import { startUpdating } from "./lib/stores";
+
   import "./style.css";
+
+  export let name;
+  export let selectedCrime: Crime;
+  export let centerMap = false;
+
+  onMount(async function () {
+    startUpdating();
+  });
+
+  function listClickHandler(evt, c) {
+    if (selectedCrime == evt.detail || evt.detail === false) {
+      selectedCrime = undefined; // toggle
+    } else {
+      centerMap = c;
+      selectedCrime = evt.detail;
+    }
+  }
 </script>
 
-<EsriMapView />
+<header>
+  {name}
+  <span><Info {name} /></span>
+</header>
+<EsriMapView
+  {selectedCrime}
+  {centerMap}
+  on:selected={(e) => {
+    listClickHandler(e, false);
+  }}
+/>
+<aside id="sidebar">
+  <List
+    {selectedCrime}
+    on:click={(e) => {
+      listClickHandler(e, true);
+    }}
+  />
+</aside>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  header {
+    grid-column: 1 / span 5;
+    font-size: 40px;
+    padding: 10px;
+    overflow: hidden;
+  }
+  header span {
+    float: right;
+  }
+
+  aside {
+    grid-column: span 2;
+    overflow-y: scroll;
+  }
+
+  @media (max-width: 700px) {
+    aside {
+      grid-column: 1 / span 5;
+    }
   }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import App from './App.svelte'
 
 const app = new App({
-  target: document.getElementById('app')
+  target: document.getElementById('app'),
+  props: {
+    name: 'St. Louis Live Crime'
+  }
 })
 
 export default app
