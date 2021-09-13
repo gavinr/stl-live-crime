7e3b26668fa6e92845d8b93c6f3b363a229c7380
gavinr <gavinr@users.noreply.github.com>
2021-09-13 05:12:39 +0000

    Deploying to gh-pages from @ gavinr/stl-live-crime@35ca8b00a6b054c1e2eec9655ad4c1810bc99223 🚀

--- assets/jsonContext.125496a3.js
+++
@@ -1,1 +0,0 @@
-import{cF as r,cI as t}from"./vendor.982f672a.js";function e(e){return{origin:"portal-item",url:r(e.itemUrl),portal:e.portal||t.getDefault(),portalItem:e,readResourcePaths:[]}}export{e as o};
---
+++ assets/scaleUtils.2d1b2a19.js
@@ -0,0 +1,1 @@
+import{h_ as t,cy as e}from"./vendor.0f6f771b.js";function n(n,r){const o=r||n.extent,f=n.width,i=e(o&&o.spatialReference);return o&&f?o.width/f*i*t*96:0}export{n as r};
